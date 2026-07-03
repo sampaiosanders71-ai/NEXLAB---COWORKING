aa6fa1e16c5704768cf587dcdd8dfd1a28ab9bd0
sampaiosanders71-ai <sampaiosanders71@gmail.com>
2026-07-02 21:03:00 -0300

    Delete scripts directory

--- scripts/generate-vapid.mjs
+++
@@ -1,7 +0,0 @@
-import webpush from "web-push";
-
-const keys = webpush.generateVAPIDKeys();
-console.log("\nVITE_VAPID_PUBLIC_KEY=" + keys.publicKey);
-console.log("VAPID_PUBLIC_KEY=" + keys.publicKey);
-console.log("VAPID_PRIVATE_KEY=" + keys.privateKey);
-console.log("\nGuarde a chave privada apenas nos secrets da Edge Function.\n");
