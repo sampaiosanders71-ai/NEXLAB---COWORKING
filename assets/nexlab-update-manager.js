e0a56937c67366af22240a0b7a0e6f7f85c76786
sampaiosanders71-ai <sampaiosanders71@gmail.com>
2026-07-16 11:35:59 -0300

    Delete assets directory

--- assets/nexlab-update-manager.js
+++
@@ -1,4 +0,0 @@
-(function(){
-  'use strict';
-  window.__NEXLAB_UPDATE_MANAGER__={version:'26.30.12',revision:'pending-contract-dom-telemetry-r5552',check:async()=>({ok:true,updateAvailable:false})};
-})();
